e3cf229b852338ae200ed9a6e061d53b6cceb386
hitendraquessou <sanjay_quessou@yahoo.co.uk>
2024-06-03 20:15:38 +0100

    feat: Implemented Parse the JSON response with kotlinx.serialization

--- app/src/main/java/com/example/marsphotos/ui/screens/MarsViewModel.kt
+++ app/src/main/java/com/example/marsphotos/ui/screens/MarsViewModel.kt
@@ -22,10 +22,17 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.marsphotos.network.MarsApi
 import kotlinx.coroutines.launch
-
+sealed interface MarsUiState{
+    data class Success(val photos: String): MarsUiState
+    object Error: MarsUiState
+    object Loading: MarsUiState
+}
+/**
+ * The [ViewModel] that is attached to the [MarsPhotosScreen]
+ */
 class MarsViewModel : ViewModel() {
     /** The mutable State that stores the status of the most recent request */
-    var marsUiState: String by mutableStateOf("")
+    var marsUiState: MarsUiState by mutableStateOf(MarsUiState.Loading)
         private set
 
     /**
@@ -41,8 +48,14 @@ class MarsViewModel : ViewModel() {
      */
     private fun getMarsPhotos() {
         viewModelScope.launch {
-            val listResult = MarsApi.retrofitService.getPhotos()
-            marsUiState = listResult
+           marsUiState = try {
+                val listResult = MarsApi.retrofitService.getPhotos()
+                MarsUiState.Success(
+                    "Success: ${listResult.size} Mars photos retrieved"
+                )
+            } catch (e: Exception) {
+                MarsUiState.Error
+            }
         }
     }
 }
